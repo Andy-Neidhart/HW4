8464d7546b5f249ce3439c9264a5dd6b6e31af20
Andy-Neidhart <132530936+Andy-Neidhart@users.noreply.github.com>
2023-10-09 18:51:43 -0700

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,4 +1,4 @@
-def merge_list(list1, list2)
+def merge_list(list1, list2):
   merged_list = list1 + list2
       for i in range(len(merged_list)):
           for j in range(i + 1, len(merged_list)):
