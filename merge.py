b888a949d96e376c4c1f2701771023776f514160
Andy-Neidhart <132530936+Andy-Neidhart@users.noreply.github.com>
2023-10-09 19:54:29 -0700

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,10 +1,11 @@
 def merge_lists(list1, list2):
     merged_list = list1 + list2
     for i in range(1, len(merged_list)):
-        key = merged_list[i]
-        j = i - 1
-        while j >= 0 and key < merged_list[j]:
-            merged_list[j + 1] = merged_list[j]
-            j -= 1
-        merged_list[j + 1] = key
+        min_index = i
+        for j in range(i + 1, size):
+            # select the minimum element in every iteration
+            if array[j] < array[min_index]:
+                min_index = j
+         # swapping the elements to sort the array
+        (array[i], array[min_index]) = (array[min_index], array[i])
     return merged_list
