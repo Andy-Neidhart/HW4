7d6816adef12874df1390353ca96930c89a9c05f
Andy-Neidhart <132530936+Andy-Neidhart@users.noreply.github.com>
2023-10-09 19:06:36 -0700

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,8 +1,8 @@
 def merge_list(list1, list2):
-  merged_list = list1 + list2
-      for i in range(len(merged_list)):
-          for j in range(i + 1, len(merged_list)):
-              if merged_list[i] > merged_list[j]:
-                  temp = merged_list[i]
-                  merged_list[i], merged_list[j] = merged_list[j], temp
-      return merged_list
+    merged_list = list1 + list2
+    for i in range(len(merged_list)):
+        for j in range(i + 1, len(merged_list)):
+            if merged_list[i] > merged_list[j]:
+                temp = merged_list[i]
+                merged_list[i], merged_list[j] = merged_list[j], temp
+        return merged_list
