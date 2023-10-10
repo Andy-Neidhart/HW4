8cd2e99132a8a909218e0d18dd536b2db9aafa2d
Andy-Neidhart <132530936+Andy-Neidhart@users.noreply.github.com>
2023-10-09 19:57:40 -0700

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,11 +1,38 @@
-def merge_lists(list1, list2):
+def merge_list(list1, list2):
+    # Check if input lists are valid
+    if not all(isinstance(num, int) for num in list1) or not all(isinstance(num, int) for num in list2):
+        raise TypeError("Input lists should only contain integers.")
+    
+    # Merge the two lists
     merged_list = list1 + list2
-    for i in range(1, len(merged_list)):
-        min_index = i
-        for j in range(i + 1, size):
-            # select the minimum element in every iteration
-            if array[j] < array[min_index]:
-                min_index = j
-         # swapping the elements to sort the array
-        (array[i], array[min_index]) = (array[min_index], array[i])
-    return merged_list
+    
+    # Perform merge sort without using built-in sort functions
+    def merge_sort(arr):
+        if len(arr) <= 1:
+            return arr
+        
+        mid = len(arr) // 2
+        left = merge_sort(arr[:mid])
+        right = merge_sort(arr[mid:])
+        
+        return merge(left, right)
+    
+    def merge(left, right):
+        merged = []
+        left_idx, right_idx = 0, 0
+        
+        while left_idx < len(left) and right_idx < len(right):
+            if left[left_idx] < right[right_idx]:
+                merged.append(left[left_idx])
+                left_idx += 1
+            else:
+                merged.append(right[right_idx])
+                right_idx += 1
+        
+        merged.extend(left[left_idx:])
+        merged.extend(right[right_idx:])
+        return merged
+    
+    # Sort and return the merged list
+    sorted_list = merge_sort(merged_list)
+    return sorted_list
